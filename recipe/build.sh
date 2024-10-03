ed364816828e13a4460fe2577460018bbf3a5b35
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-10-03 20:06:59 +0900

    Change build format

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e -x
 
-# Cf. https://github.com/conda-forge/staged-recipes/issues/673, we're in the
-# process of excising Libtool files from our packages. Existing ones can break
-# the build while this happens. We have "/." at the end of $PREFIX to be safe
-# in case the variable is empty.
-find ${PREFIX}/. -name '*.la' -delete
-
 autoreconf_args=(
     --force
     --verbose
@@ -16,28 +10,22 @@ autoreconf_args=(
 )
 autoreconf "${autoreconf_args[@]}"
 
-export CONFIG_FLAGS="--build=${BUILD}"
-
 export PKG_CONFIG_LIBDIR="${PREFIX}/lib/pkgconfig:${PREFIX}/share/pkgconfig"
 configure_args=(
-    ${CONFIG_FLAGS}
+    --prefix="${PREFIX}"
+    --disable-static
     --disable-debug
     --disable-dependency-tracking
     --disable-selective-werror
     --disable-silent-rules
-    --prefix="${PREFIX}"
 )
 
-if [ "${CONDA_BUILD_CROSS_COMPILATION}" = "1" ] ; then
-    configure_args+=(
-        --enable-malloc0returnsnull
-    )
-fi
-
 ./configure "${configure_args[@]}"
-make -j${CPU_COUNT}
+make -j$CPU_COUNT
 make install
-
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+make check
+fi
 rm -rf ${PREFIX}/share/man ${PREFIX}/share/doc/${PKG_NAME}
 
 # Remove any new Libtool files we may have installed. It is intended that
